4919b85936b881f02dd8c4b0ecad1dec4fa45652
Axeniter <anamievoid@gmail.com>
2025-12-08 20:14:19 +1000

    Feat: card class

---
+++ Poker/Models/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.Models
+{
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
+    }
+
+    public enum Rank
+    {
+        Two=2, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+        Jack, Queen, King, Ace
+    }
+
+    public class Card
+    {
+        public Suit Suit { get; }
+        public Rank Rank { get; }
+
+        public Card(Suit suit, Rank rank)
+        { 
+            Suit = suit;
+            Rank = rank;
+        }
+    }
+}
